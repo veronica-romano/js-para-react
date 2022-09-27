267943151140c8156e0f1c418cc6905bfe1c01ab
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-09-27 11:09:02 -0300

    refatorando funções
    para arrow function

--- 07-rest.js
+++ 07-rest.js
@@ -1,8 +1,26 @@
 /* mesclar uma lista de argumentos ou parâmetros para uma função */
 
-function ordenar(...parametros) {
+/* 
+sem arrow
+function ordenar = (...parametros) => {
     return parametros.sort();
-}
+} */
 
+//com arrow
+const ordenar = (...parametros) => parametros.sort();
+
+const bandas = ['Dommu Borgir', 'Batoushka', 'Mayhem', 'Cradle of Filth', 'Venom']
 console.log(ordenar('Veronica', 'Guilherme', 'Glauber'));
 console.log(ordenar('Dommu Borgir', 'Batoushka', 'Mayhem', 'Cradle of Filth', 'Venom'));
+console.log(ordenar(...bandas));
+console.log(ordenar(...['Dommu Borgir', 'Batoushka', 'Mayhem', 'Cradle of Filth', 'Venom']));
+
+const soma = (...valores) => {
+    let total = 0;
+    for (let valor of valores) {
+        total += valor;
+    }
+    return total;
+}
+
+console.log(soma(0,1,2,3,4,5,6,7,8,9));
